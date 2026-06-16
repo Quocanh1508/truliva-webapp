c570e11865921621250b43d1a4911362081ad77c
QUOC ANH <quocanh0815@gmail.com>
2026-06-16 21:28:52 +0700

    fix(android): force CookieManager to flush to disk onPause to prevent session loss

--- webapp/android/app/src/main/java/com/truliva/app/MainActivity.java
+++ webapp/android/app/src/main/java/com/truliva/app/MainActivity.java
@@ -1,5 +1,14 @@
 package com.truliva.app;
 
+import android.os.Bundle;
+import android.webkit.CookieManager;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onPause() {
+        super.onPause();
+        CookieManager.getInstance().flush();
+    }
+}
+
