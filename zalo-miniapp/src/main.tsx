a4c1afa9ed2f0c24ea3e3c01a60b795f655f67fc
QUOC ANH <quocanh0815@gmail.com>
2026-07-20 11:28:35 +0700

    feat: complete Zalo Mini App build setup, index.html, and entrypoint

---
+++ zalo-miniapp/src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import IndexPage from './pages/index';
+
+ReactDOM.createRoot(document.getElementById('app')!).render(
+  <React.StrictMode>
+    <IndexPage />
+  </React.StrictMode>
+);
