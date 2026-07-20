1016823f6a933010206ce259dc180db9d52acf56
QUOC ANH <quocanh0815@gmail.com>
2026-07-20 13:59:30 +0700

    fix: integrate ZMPRouter and AnimationRoutes to signal page mount to Zalo native webview container

--- zalo-miniapp/src/main.tsx
+++ zalo-miniapp/src/main.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App, Page } from 'zmp-ui';
+import { App, ZMPRouter, AnimationRoutes, Page } from 'zmp-ui';
+import { Route } from 'react-router-dom';
 import 'zmp-ui/zaui.css';
 import IndexPage from './pages/index';
 
+const MyApp = () => {
+  return (
+    <App>
+      <ZMPRouter>
+        <AnimationRoutes>
+          <Route path="/" element={<Page><IndexPage /></Page>} />
+        </AnimationRoutes>
+      </ZMPRouter>
+    </App>
+  );
+};
+
 ReactDOM.createRoot(document.getElementById('app')!).render(
   <React.StrictMode>
-    <App>
-      <Page>
-        <IndexPage />
-      </Page>
-    </App>
+    <MyApp />
   </React.StrictMode>
 );
