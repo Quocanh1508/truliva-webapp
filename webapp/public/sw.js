20f1bba8907a70b7a4a80b76fbafe0e0589f85b2
QUOC ANH <quocanh0815@gmail.com>
2026-07-30 10:42:21 +0700

    fix(sw): clear old caches on activate to prevent stale PWA assets

--- webapp/public/sw.js
+++ webapp/public/sw.js
@@ -3,7 +3,10 @@ self.addEventListener('install', (event) => {
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.map((k) => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', (event) => {
